32bab88c69354b727aba637f3205d4f28a3870b7
63070055 <73680542+63070055@users.noreply.github.com>
2023-07-07 23:14:55 +0700

    Add files via upload

---
+++ back-end/service/task.go
@@ -0,0 +1,25 @@
+package service
+
+import "time"
+
+type TaskResponse struct {
+	ID       int       `json:"id"`
+	Taskname string    `json:"taskname"`
+	Title    string    `json:"title"`
+	Time     time.Time `json:"time"`
+	Check    string    `json:"check"`
+}
+type TaskRequest struct {
+	Taskname string `json:"taskname"`
+	Title    string `json:"title"`
+	Check    string `json:"check"`
+}
+
+// เลือกแค่ต้องการให้แสดงผล
+type TaskService interface {
+	GetTaskAllCos() ([]TaskResponse, error)
+	GetTaskByIdCos(id int) (*TaskResponse, error)
+	CreateTaskCos(task TaskRequest) (*TaskResponse, error)
+	UpdateTaskCos(id int, task TaskRequest) (*TaskResponse, error)
+	DeleteTaskCos(id int) error
+}
